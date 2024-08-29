db4dfa216d20505555cc7e0b4a2241dbbfbc308b
LauraPosadaGordillo <lauraposadagordillo@gmail.com>
2024-08-28 23:29:42 -0500

    Update ejercicio4.py

--- Listas/ejercicio4.py
+++ Listas/ejercicio4.py
@@ -1,5 +1,6 @@
 #las istas se definen con corchrtes
 mi_lista = ["l", "e", "m"]
+
 # len sirvee para contar los elementos de una lista o cadena de caracteres
 resultado = len(mi_lista)
 print(resultado)
